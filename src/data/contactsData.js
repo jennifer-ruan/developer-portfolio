1454d2d4b4043c3ef55eaf7c1bfc31b69abe4296
Jennifer Ruan <jennifer.y.ruan@gmail.com>
2023-01-19 02:44:45 -0500

    remove phone

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,6 @@
 export const contactsData = {
-    email: 'jennifer.y.ruan@gmail.com',
-    phone: '+16478782250',
-    address: 'Toronto, ON',
+  email: "jennifer.y.ruan@gmail.com",
+  address: "Toronto, ON",
 
-    sheetAPI: ''
-}
+  sheetAPI: "",
+};
